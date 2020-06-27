2ecc6707451c7e107d75e792722ae41b85832a8d
Raymond To <rto@demandbase.com>
2020-06-27 14:12:46 -0700

    break and session config ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <h1>Hello Raymond</h1>;
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+  const decrementBreakLength = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
+
+  const incrementBreakLength = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  const decrementSessionLength = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+    }
+  };
+
+  const incrementSessionLength = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1 data-testid="break-label">Break Length</h1>
+      <button type="button" data-testid="break-decrement" onClick={decrementBreakLength}>down</button>
+      <div data-testid="break-length">{breakLength}</div>
+      <button type="button" data-testid="break-increment" onClick={incrementBreakLength}>up</button>
+
+      <h1 data-testid="session-label">Session Length</h1>
+      <button type="button" data-testid="session-decrement" onClick={decrementSessionLength}>down</button>
+      <div data-testid="session-length">{sessionLength}</div>
+      <button type="button" data-testid="session-increment" onClick={incrementSessionLength}>up</button>
+
+      <h1 data-testid="timer-label">Session</h1>
+      <div data-testid="time-left">25:00</div>
+    </div>
+  );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,125 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/hello raymond/i)).toBeInTheDocument();
+test('break length', () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('heading', { name: /^Break Length$/ })).toBeInTheDocument();
+});
+
+test('session length', () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('heading', { name: /^Session Length$/ })).toBeInTheDocument();
+});
+
+test('break decrement button', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('break-decrement')).toHaveTextContent('down');
+});
+
+test('break increment button', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('break-increment')).toHaveTextContent('up');
+});
+
+test('session decrement button', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('session-decrement')).toHaveTextContent('down');
+});
+
+test('session increment button', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('session-increment')).toHaveTextContent('up');
+});
+
+test('break length display', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('break-length')).toHaveTextContent('5');
+
+  fireEvent.click(getByTestId('break-decrement'));
+  expect(getByTestId('break-length')).toHaveTextContent('4');
+  fireEvent.click(getByTestId('break-decrement'));
+  expect(getByTestId('break-length')).toHaveTextContent('3');
+
+  fireEvent.click(getByTestId('break-increment'));
+  expect(getByTestId('break-length')).toHaveTextContent('4');
+  fireEvent.click(getByTestId('break-increment'));
+  expect(getByTestId('break-length')).toHaveTextContent('5');
+});
+
+test('break length display should not go below 0', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('break-length')).toHaveTextContent('5');
+
+  for (let i = 5; i > 1; i -= 1) {
+    fireEvent.click(getByTestId('break-decrement'));
+  }
+  expect(getByTestId('break-length')).toHaveTextContent('1');
+
+  fireEvent.click(getByTestId('break-decrement'));
+  expect(getByTestId('break-length')).toHaveTextContent('1');
+});
+
+test('break length display should not go above 60', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('break-length')).toHaveTextContent('5');
+
+  for (let i = 5; i < 60; i += 1) {
+    fireEvent.click(getByTestId('break-increment'));
+  }
+  expect(getByTestId('break-length')).toHaveTextContent('60');
+
+  fireEvent.click(getByTestId('break-increment'));
+  expect(getByTestId('break-length')).toHaveTextContent('60');
+});
+
+test('session length display', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('session-length')).toHaveTextContent('25');
+
+  fireEvent.click(getByTestId('session-decrement'));
+  expect(getByTestId('session-length')).toHaveTextContent('24');
+  fireEvent.click(getByTestId('session-decrement'));
+  expect(getByTestId('session-length')).toHaveTextContent('23');
+
+  fireEvent.click(getByTestId('session-increment'));
+  expect(getByTestId('session-length')).toHaveTextContent('24');
+  fireEvent.click(getByTestId('session-increment'));
+  expect(getByTestId('session-length')).toHaveTextContent('25');
+});
+
+test('session length display should not go below 0', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('session-length')).toHaveTextContent('25');
+
+  for (let i = 25; i > 1; i -= 1) {
+    fireEvent.click(getByTestId('session-decrement'));
+  }
+  expect(getByTestId('session-length')).toHaveTextContent('1');
+
+  fireEvent.click(getByTestId('session-decrement'));
+  expect(getByTestId('session-length')).toHaveTextContent('1');
+});
+
+test('session length display should not go above 60', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('session-length')).toHaveTextContent('5');
+
+  for (let i = 25; i < 60; i += 1) {
+    fireEvent.click(getByTestId('session-increment'));
+  }
+  expect(getByTestId('session-length')).toHaveTextContent('60');
+
+  fireEvent.click(getByTestId('session-increment'));
+  expect(getByTestId('session-length')).toHaveTextContent('60');
+});
+
+test('timer label', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('timer-label')).toHaveTextContent('Session');
+});
+
+test('time left', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('time-left')).toHaveTextContent('25:00');
 });
